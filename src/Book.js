aa7855d03f3a2f970507e5f135c1c20ea63cb347
Kevin Ziegler <kevin.ziegler@bigcommerce.com>
2019-03-21 13:51:52 -0500

    feat: Allow an onMove handler to be passed to the 'Book' component
    This is passed through to the Bookshelf Changer component.

--- src/Book.js
+++ src/Book.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import BookshelfChanger from './BookshelfChanger';
 
-const Book = (props) => {
+const Book = ({book, onMove}) => {
     return (
         <li>
             <div className="book">
                 <div className="book-top">
                     <div
-                      className="book-cover"
-                      style={{
-                          width: 128,
-                          height: 193,
-                          backgroundImage: `url(${ props.book.imageLinks.smallThumbnail })`
-                      }}
+                        className="book-cover"
+                        style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url(${ book.imageLinks.smallThumbnail })`
+                        }}
                     ></div>
-                    <BookshelfChanger />
+                    <BookshelfChanger book={book} onMove={onMove} />
                 </div>
-              <div className="book-title">{props.book.title}</div>
-              <div className="book-authors">{props.book.authors.join(', ')}</div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors.join(', ')}</div>
             </div>
         </li>
     );
