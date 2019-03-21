bb5aacd80ad6c2e535f9dc833c44a7ef9812cec3
Kevin Ziegler <kevin.ziegler@bigcommerce.com>
2019-03-21 13:54:18 -0500

    feat: Add 'onMove' handler to the 'Shelf' component
    This is passed to all books on the shelf, allowing proper propagation of changes
    when updating a book's location.

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Book from './Book';
 
-const Shelf = (props) => {
+const Shelf = ({name, books, onMove}) => {
     return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">{ props.name }</h2>
+            <h2 className="bookshelf-title">{name}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    { props.books.map((book) => (<Book book={ book } />)) }
+                    {books.map((book) => (<Book book={book} onMove={onMove} />))}
                 </ol>
             </div>
         </div>
-    )
+    );
 };
 
 export default Shelf;
