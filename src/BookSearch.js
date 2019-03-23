0d247d2809d3e5b3876e64adefc64a2a48d7dcf7
Kevin Ziegler <kevin.ziegler@bigcommerce.com>
2019-03-23 12:20:05 -0500

    feat: Make the booksearch page actually search for books

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,30 +1,89 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Book from './Book';
+import { search } from './BooksAPI';
 
-const BookSearch = (props) => (
-    <div className="search-books">
-        <div className="search-books-bar">
-            <Link to='/'>
-                <button className="close-search">Close</button>
-            </Link>
+class BookSearch extends React.Component {
+    constructor() {
+        super();
 
-            <div className="search-books-input-wrapper">
-            {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        this.emptyMessage = this.emptyMessage.bind(this);
+        this.renderResults = this.renderResults.bind(this);
+        this.searchBooks = this.searchBooks.bind(this);
+        this.processResults = this.processResults.bind(this);
 
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-            */}
-                <input type="text" placeholder="Search by title or author"/>
+        this.state = { query: '', results: [] };
+    }
 
+    emptyMessage() {
+        const message = (this.state.query.length === 0)
+            ? 'Enter a query above to search for books'
+            : 'No results found!';
+
+        return (<h3>{message}</h3>);
+    }
+
+    renderResults() {
+        return (
+            <ol className="books-grid">
+                { this.state.results.map(book => (
+                    <Book book={book} onMove={() => {}}/>
+                ))}
+            </ol>
+        );
+    }
+
+    searchBooks(event) {
+        const query = event.target.value;
+        event.preventDefault();
+        this.setState({ query });
+
+        search(query).then(this.processResults);
+    }
+
+    processResults(results) {
+        results = ((results || {}).error || this.state.query.length === 0)
+            ? []
+            : results;
+
+        this.setState({ results });
+    }
+
+    render() {
+        return  (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to='/'>
+                        <button className="close-search">Close</button>
+                    </Link>
+
+                    <div className="search-books-input-wrapper">
+                        {/*
+                            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                            You can find these search terms here:
+                            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                            you don't find a specific author or title. Every search is limited by search terms.
+                        */}
+                        <input
+                          type="text"
+                          placeholder="Search by title or author"
+                          value={this.state.query}
+                          onChange={this.searchBooks}
+                        />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    {
+                        (this.state.results.length === 0)
+                            ? this.emptyMessage()
+                            : this.renderResults()
+                    }
+                </div>
             </div>
-        </div>
-        <div className="search-books-results">
-            <ol className="books-grid"></ol>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default BookSearch;
